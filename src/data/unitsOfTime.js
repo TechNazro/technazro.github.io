50d624aca087c15a649549d402043453e5d3ce2a
Pruthviraj <technazro@gmail.com>
2023-10-07 11:30:22 +0530

    created javascript file of units of time of sanatana dharma

---
+++ src/data/unitsOfTime.js
@@ -0,0 +1,76 @@
+const manvantara = `
+In Hindu cosmology, a manvantara is a cyclic period of time identifying the duration, reign, or age of a Manu, the progenitor of mankind. In each manvantara, seven Rishis, certain deities, an Indra, a Manu, and kings (sons of Manu) are created and perish. Each manvantara is distinguished by the Manu who rules/reigns over it, of which we are currently in the seventh manvantara of fourteen, which is ruled by Vaivasvata Manu.
+
+The word "manvantara" is a combination of the Sanskrit words "manu" (man, mankind, or Manu, the progenitor of mankind) and "antara" (interval, period, or term), creating manu-antara or manvantara, literally meaning "the duration of a Manu", or his lifespan.
+
+The length of a manvantara is said to be 71 mahayugas, or 4.32 billion years. Each mahayuga is divided into four yugas, or ages:
+
+* Krita Yuga: The golden age, when humanity is virtuous and prosperous.
+* Treta Yuga: The silver age, when humanity is less virtuous and prosperity begins to decline.
+* Dvapara Yuga: The copper age, when humanity is even less virtuous and prosperity continues to decline.
+* Kali Yuga: The iron age, when humanity is very sinful and prosperity is at its lowest.
+
+At the end of each manvantara, a great flood destroys the world. The Manu and the seven Rishis are the only survivors, who then repopulate the world.
+
+The current manvantara, the Vaivasvata Manvantara, is said to have begun 5.02 billion years ago and is expected to last for another 4.32 billion years. The next manvantara will be the Satyavrata Manvantara, which will be ruled by Satyavrata Manu.
+`;
+
+
+const manu = `Manu is a term found as various meanings in Hinduism. In early texts, it refers to the archetypal man, or to the first man (progenitor of humanity). The Sanskrit term for 'human', मानव (IAST: mānava) means 'of Manu' or 'children of Manu'. In later texts, Manu is the title or name of fourteen rulers of earth, or alternatively as the head of dynasties that begin with each cyclic kalpa (aeon) when the universe is born anew.
+
+The current Manu is Vaivasvata Manu, the seventh of the 14 Manus of the current kalpa (aeon) of Hindu cosmology. He is the son of Vivasvana (also known as Surya), the Sun god. Forewarned about the divine flood by the Matsya avatara of the god Vishnu, Manu saved mankind by building a boat that carried his family and the Saptarishi to safety.
+
+The duration of a Manu's reign is called a manvantara. A manvantara is said to last 71 yugas, or 306,720,000 human years. The current manvantara is the seventh of the 14 manvantaras of the current kalpa.
+
+Therefore, the duration of Manu is 306,720,000 human years. Since the current kalpa is only halfway through, Manu has still about half of his reign left.
+
+It is important to note that the concept of Manu and the manvantaras is found in Hindu mythology and cosmology, and is not based on historical or scientific evidence.`;
+
+
+const kalpa = `
+Kalpa (Sanskrit: कल्प) is a long period of time in Hindu and Buddhist cosmology, generally between the creation and recreation of a world or universe. The length of a kalpa varies by tradition. In the "Puranas," it is described as 4.32 billion years, while Buddhism cites four kalpas of different lengths, from roughly 16 million years to about 1.28 trillion years.
+
+A kalpa is divided into 14 manvantara periods, each lasting 71 Yuga Cycles (306,720,000 years). Preceding the first and following each manvantara period is a juncture (sandhya) equal to the length of a Satya Yuga (1,728,000 years). A kalpa is followed by a pralaya (dissolution) of equal length, which together constitute a day and night of Brahma. A month of Brahma contains thirty such days and nights, or 259.2 billion years.
+
+The current kalpa is the Vaivasvata Kalpa, which is said to have begun 5.02 billion years ago and is expected to last for another 4.32 billion years.
+
+The concept of kalpa is used to explain the cyclical nature of time and the universe. It is believed that the universe is created and destroyed over and over again, in an endless cycle. Each kalpa is a new beginning, but the basic laws of the universe remain the same.
+
+The concept of kalpa is also used to teach about the impermanence of all things. Even the universe itself is not permanent. It will eventually be destroyed, and then created anew. This teaches us to appreciate the present moment and to make the most of our time here on Earth.
+`;
+
+
+const yugasAndMahayuga = `
+The four yugas and mahayuga are a concept in Hindu cosmology that describes the cyclical nature of time and the universe. There are four yugas in a cycle: Krita Yuga, Treta Yuga, Dvapara Yuga, and Kali Yuga. Each yuga is progressively shorter than the preceding one, and the moral and spiritual state of humanity is said to decline in each yuga.
+
+The four yugas together make up a Yuga Cycle, which lasts for 4,320,000 years. The current yuga is the Kali Yuga, which is said to have begun in 3102 BCE and is expected to last for 432,000 years.
+
+Here is a more detailed explanation of each yuga and its duration:
+
+* Krita Yuga : (4,800 divine years, or 1,728,000 human years)
+
+The Krita Yuga is the golden age, when humanity is virtuous and prosperous. People are naturally good and there is no crime or violence. Everyone is happy and healthy. The weather is perfect and the crops grow abundantly.
+
+* Treta Yuga : (3,600 divine years, or 1,296,000 human years)
+
+The Treta Yuga is the silver age, when humanity is less virtuous and prosperity begins to decline. People start to become more selfish and greedy. Crime and violence increase. The weather becomes less perfect and the crops start to grow less abundantly.
+
+* Dvapara Yuga : (2,400 divine years, or 864,000 human years)
+
+The Dvapara Yuga is the copper age, when humanity is even less virtuous and prosperity continues to decline. People become more and more sinful. Crime and violence are rampant. The weather becomes even less perfect and the crops grow even less abundantly.
+
+* Kali Yuga :  (1,200 divine years, or 432,000 human years)
+
+The Kali Yuga is the iron age, when humanity is very sinful and prosperity is at its lowest. People are selfish, greedy, and violent. There is no morality or spirituality. The weather is terrible and the crops barely grow at all.
+
+A mahayuga is a period of 4,320,000 years, or 12,000 divine years. It consists of four yugas in the following order:
+
+* Krita Yuga
+* Treta Yuga
+* Dvapara Yuga
+* Kali Yuga
+
+At the end of each mahayuga, the universe is said to be destroyed and then recreated anew.
+
+The concept of yugas and mahayuga is a complex and profound one. It has been interpreted and reinterpreted by Hindu scholars for centuries. It is a way of understanding the cyclical nature of time and the universe, as well as the impermanence of all things.
+`;
